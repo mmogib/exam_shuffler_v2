1d58659a5119d08dab9839530acefbf25782d6ac
Mohammed Alshahrani <mmogib@gmail.com>
2019-01-10 13:18:03 +0300

    added hover cards

--- src/components/SaveDownloadExam.vue
+++ src/components/SaveDownloadExam.vue
@@ -1,56 +1,88 @@
 <template>
   <v-layout row wrap>
     <v-flex xs6 class="pa-1">
-      <v-card color="teal lighten-2" class="white--text">
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Save</div>
-            <span>Save this exam to a file</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat dark @click="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-hover>
+        <v-card
+          slot-scope="{ hover }"
+          :class="`elevation-${hover ? 12 : 2} clickable`"
+          color="teal lighten-2"
+          class="white--text"
+          @click="save"
+        >
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Save</div>
+              <span>Save this exam to a file</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat dark @click="save">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-hover>
     </v-flex>
     <v-flex xs6 class="pa-1">
-      <v-card color="purple darken-1" class="white--text">
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Download</div>
-            <span>Download the shuffled latex file.</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat dark @click="download">Download</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-hover>
+        <v-card
+          color="purple darken-1"
+          class="white--text"
+          slot-scope="{ hover }"
+          :class="`elevation-${hover ? 12 : 2} clickable`"
+          @click="download"
+        >
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Download</div>
+              <span>Download the shuffled latex file.</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat dark @click="download">Download</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-hover>
     </v-flex>
     <v-flex xs6 class="pa-1">
-      <v-card color="purple accent-2" class="white--text">
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Make default</div>
-            <span>Save this exam as a default exam</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat dark @click="savedefault">Make Default</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-hover>
+        <v-card
+          color="purple accent-2"
+          class="white--text"
+          slot-scope="{ hover }"
+          :class="`elevation-${hover ? 12 : 2} clickable`"
+          @click="savedefault"
+        >
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Make default</div>
+              <span>Save this exam as a default exam</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat dark @click="savedefault">Make Default</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-hover>
     </v-flex>
     <v-flex xs6 class="pa-1">
-      <v-card color="green darken-2" class="white--text">
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Start over</div>
-            <span>to start the exam preperation</span>
-          </div>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn flat dark @click="startover">Start Over</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-hover>
+        <v-card
+          color="green darken-2"
+          class="white--text"
+          slot-scope="{ hover }"
+          :class="`elevation-${hover ? 12 : 2} clickable`"
+          @click="startover"
+        >
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">Start over</div>
+              <span>to start the exam preperation</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat dark @click="startover">Start Over</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-hover>
     </v-flex>
     <v-flex xs12 class="pa-1">
       <loading v-if="isBusy"/>
@@ -88,4 +120,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+</style>
 
